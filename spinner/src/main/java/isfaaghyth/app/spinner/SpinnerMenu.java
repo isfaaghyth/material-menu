b4fe86e18006dcf7f20aedec5e50832fadf0f385
Muh.Isfahani Ghiyath.YM <isfaaghyth@users.noreply.github.com>
2018-12-22 17:01:45 +0700

    90% of progress in SpinnerMenu class

--- spinner/src/main/java/isfaaghyth/app/spinner/SpinnerMenu.java
+++ spinner/src/main/java/isfaaghyth/app/spinner/SpinnerMenu.java
@@ -2,7 +2,18 @@ package isfaaghyth.app.spinner;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+import isfaaghyth.app.spinner.adapter.DropdownAdapter;
+import isfaaghyth.app.spinner.util.ItemContent;
+import isfaaghyth.app.spinner.util.MenuItemListener;
 
 /**
  * Created by isfaaghyth on 22/12/18.
@@ -10,16 +21,109 @@ import android.widget.RelativeLayout;
  */
 public class SpinnerMenu extends RelativeLayout {
 
+    private Context context;
+
+    /**
+     * each views of view_main
+     */
+    private TextView txtItem;
+    private TextView txtSubItem;
+    private ImageView icArrow;
+    private ListView lstDropdown;
+    private View currentItem;
+
+    private MenuItemListener listener;
+
+    /**
+     * flag to show up/down of dropdown list
+     */
+    private boolean isCurrentClicked = false;
+
+    private View createView(Context context) {
+        View viewMain = LayoutInflater.from(context).inflate(R.layout.view_main, null);
+
+        //root view (= current item)
+        currentItem = viewMain.findViewById(R.id.current_item);
+
+        txtItem = viewMain.findViewById(R.id.txt_item);
+        txtSubItem = viewMain.findViewById(R.id.txt_sub_item);
+        lstDropdown = viewMain.findViewById(R.id.lst_menu);
+        icArrow = viewMain.findViewById(R.id.ic_arrow);
+
+        return viewMain;
+    }
+
     public SpinnerMenu(Context context) {
         super(context);
+        this.context = context;
+        addView(createView(context));
     }
 
     public SpinnerMenu(Context context, AttributeSet attrs) {
         super(context, attrs);
+        this.context = context;
+        addView(createView(context));
     }
 
-    public SpinnerMenu(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+    /**
+     * insert all of data into listView
+     * @param items
+     */
+    public void setItems(List<ItemContent> items) {
+        build(items);
+        initCurrentItem(items.get(0));
     }
 
+    private boolean isCurrentClicked() {
+        if (isCurrentClicked) {
+            lstDropdown.setVisibility(View.VISIBLE);
+            icArrow.setImageResource(R.mipmap.ic_arrow_drop_down);
+            isCurrentClicked = false;
+            return false;
+        } else {
+            lstDropdown.setVisibility(View.GONE);
+            icArrow.setImageResource(R.mipmap.ic_arrow_drop_up);
+            isCurrentClicked = true;
+            return true;
+        }
+    }
+
+    private void build(List<ItemContent> items) {
+        DropdownAdapter adapter = new DropdownAdapter(items, context);
+        adapter.setListener(new MenuItemListener() {
+            @Override
+            public void onClick(ItemContent item) {
+                currentItem(item);
+                isCurrentClicked();
+                listener.onClick(item);
+            }
+        });
+        lstDropdown.setAdapter(adapter);
+    }
+
+    private void initCurrentItem(final ItemContent item) {
+        ItemContent currentItem = new ItemContent() {
+            @Override public String menuItem() {
+                return item.menuItem();
+            }
+
+            @Override public String menuSubItem() {
+                return item.menuSubItem();
+            }
+
+            @Override public int dataId() {
+                return 0;
+            }
+        };
+        currentItem(currentItem);
+    }
+
+    /**
+     * set current item
+     * @param item
+     */
+    private void currentItem(ItemContent item) {
+        txtItem.setText(item.menuItem());
+        txtSubItem.setText(item.menuSubItem());
+    }
 }
